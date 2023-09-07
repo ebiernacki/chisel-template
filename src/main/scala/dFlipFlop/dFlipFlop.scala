86bba3d8b892d8b114a74acfa4a401c674740c0d
Eric Biernacki <54508395+ebiernacki@users.noreply.github.com>
2023-09-07 00:42:48 -0500

    incomplete dFlipFlop

--- src/main/scala/dFlipFlop/dFlipFlop.scala
+++ src/main/scala/dFlipFlop/dFlipFlop.scala
@@ -5,3 +5,11 @@ import chisel3.util._
 import chisel3.experimental._
 import chisel3.experimental.BundleLiterals._
 
+class dFlipFlop extends Module{
+    val io = IO(new Bundle {
+        val d        = Input(UInt(1.W))
+        //val clk       = Input()
+        val q        = Output(UInt(1.W))
+        val nq       = Output(UInt(1.W))
+    })
+}
